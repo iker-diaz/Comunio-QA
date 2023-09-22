eb886f384cd6272b1211d554c1e1aaa03f81300f
Jorge Luis Salinas Arroyo <jorge-luis.salinas@inetum.com>
2023-09-22 10:27:03 +0200

    Version 22/09/23

--- APINodejs/src/app.js
+++ APINodejs/src/app.js
@@ -4,10 +4,11 @@ const { Client } = require('pg');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
+
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 
-// Connect to the "comunio" database
+// Conexion con base de datos
 const client = new Client({
     host: '10.228.64.234',
     port: 5432,
@@ -15,20 +16,42 @@ const client = new Client({
     user: 'postgres',
     password: 'fBA!VSSGw!8af^&2',
 });
-  
+
 client.connect();
+/*
+Endpoints
+GET /jugadores
+GET /jugador/:{id}
+GET /jugador/:{id}/general
+GET /jugador/:{id}/infoMercado
+GET /jugador/:{id}/estadistica
+GET /jugador/:{id}/:campo
+PATCH /jugador/:{id}/actualizar
+GET /jugador/:{id}/:{campo}
+Post /post/jugador
+GET /get/jugador/:{id}/puntos
+GET /get/historial_jugador/:{id}/dias/:{dias}
+GET /get/historial_jugador/:{id}/general/dias/:{dias}
+GET /get/historial_jugador/:{id}/mercado_valor/dias/:{dias}
+GET /get/historial_jugador/:{id}/puntos/dias/:{dias}
+GET /get/historial_jugador/:{id}/estadistica/dias/:{dias}
+GET /get/historial_jugador/:{id}/:campo/dias/:{dias}
+GET /get/mejor_fichaje
+*/
+
 
 // Get de todos los jugadores
 app.get('/api/v1/jugadores', (req, res) => {
-    const query = 'SELECT * FROM jugadores';
+    const query = `SELECT *
+                FROM jugadores`;
     client.query(query, (error, results) => {
         if (error) {
             console.error("Error executing MySQL query:", error);
             res.status(500).json({ error: "Internal Server Error" });
             return;
         }
-        const jugadores = results.rows;
-        res.status(200).json({ jugadores });
+        const jugador = results.rows;
+        res.status(200).json({ jugador });
     });
 });
 
@@ -36,11 +59,11 @@ app.get('/api/v1/jugadores', (req, res) => {
 // Devuelve todos los datos de un jugador buscado por su ID
 app.get('/api/v1/jugador/:id', (req, res) => {
     const id = req.params.id;
-    const query = `SELECT * 
+    const query = `SELECT *
                 FROM jugadores 
                 WHERE id_jugador = $1`;
 
-    const values = [id]
+    const values = [id];
     client.query(query, values, (error, results) => {
         if (error) {
             console.error("Error executing MySQL query:", error);
@@ -53,9 +76,9 @@ app.get('/api/v1/jugador/:id', (req, res) => {
 });
 
 // Devuelve los datos generales de un jugador buscando por su ID
-app.get("/api/v1/jugador/:id/general", (req, res) =>{
+app.get("/api/v1/jugador/:id/general", (req, res) => {
     const id = req.params.id;
-    const query = "SELECT nombre, propietario, equipo, posicion, titular, partidos_jugados, mejor_fichaje, racha, lesion FROM jugadores WHERE id_jugador = $i;";
+    const query = "SELECT nombre, propietario, equipo, posicion, titular, partidos_jugados, mejor_fichaje, racha, lesion FROM jugadores WHERE id_jugador = $1;";
     const values = [id];
     client.query(query, values, (error, results) => {
         if (error) {
@@ -86,12 +109,13 @@ app.get('/api/v1/jugador/:id/infoMercado', (req, res) => {
     })
 })
 
-// Devuelve un dato concreto de un jugador buscando por su ID
-app.get("/api/v1/jugador/:id/:campo", (req, res) =>{
+// Devuelve los campos relativos a las estadisticas del jugador buscado por ID.
+app.get('/api/v1/jugador/:id/estadistica', (req, res) => {
     const id = req.params.id;
-    const campo = req.params.campo;
-    const query = "SELECT nombre, propietario, equipo, posicion, titular, partidos_jugados, mejor_fichaje, racha, lesion FROM jugadores WHERE id_jugador = $i;";
-    const values = [id];
+    const query = `SELECT nombre, ranking_general, ranking_equipo, ranking_posicion, tarjeta_amarilla, doble_tarjeta_amarilla, tarjeta_roja
+    FROM jugadores
+    WHERE id_jugador = $1`;
+    const values = [id]
     client.query(query, values, (error, results) => {
         if (error) {
             console.error("Error executing MySQL query:", error);
@@ -139,94 +163,186 @@ app.patch('/api/v1/jugador/:id/actualizar', (req, res) => {
     });
 });
 
-
-/*
-app.put('/api/v1/jugadores/editar/:id', (req, res) => {
-    const { nombre, propietario, equipo, posicion, titular, partidos_jugados, once_ideal, evolucion_semanal, ranking_general, mejor_fichaje, puntuaje, media_softscore, media_puntos, total_puntos, puntos_buenos, oferta_minima, valor_mercado, valor_mercado_max, valor_mercado_min, ranking_equipo, ranking_posicion, goles, asistencias, paradas, tarjeta_amarilla, tarjeta_roja, doble_tarjeta_amarilla } = req.body;
+// Devuelve un dato concreto de un jugador buscando por su ID
+app.get("/api/v1/jugador/:id/:campo", (req, res) => {
     const id = req.params.id;
-    const query = `UPDATE jugadores 
-                    SET nombre = '${nombre}', propietario = '${propietario}', equipo = '${equipo}', posicion = '${posicion}', titular = '${titular}', partidos_jugados = '${partidos_jugados}', once_ideal = '${once_ideal}', evolucion_semanal = '${evolucion_semanal}', ranking_general = '${ranking_general}', mejor_fichaje = '${mejor_fichaje}', puntuaje = '${puntuaje}', media_softscore = '${media_softscore}', media_puntos = '${media_puntos}', total_puntos = '${total_puntos}', puntos_buenos = '${puntos_buenos}', oferta_minima = '${oferta_minima}', valor_mercado = '${valor_mercado}', valor_mercado_max = '${valor_mercado_max}', valor_mercado_min = '${valor_mercado_min}', ranking_equipo = '${ranking_equipo}', ranking_posicion = '${ranking_posicion}', goles = '${goles}', asistencias = '${asistencias}', paradas = '${paradas}', tarjeta_amarilla = '${tarjeta_amarilla}', tarjeta_roja = '${tarjeta_roja}', doble_tarjeta_amarilla = '${doble_tarjeta_amarilla}'
-                    WHERE id = ${id}`;
-
-    client.query(query, (error, results) => {
+    const campo = req.params.campo;
+    const query = `SELECT $2 FROM jugadores WHERE id_jugador = $1`;
+    const values = [id, campo];
+    client.query(query, values, (error, results) => {
         if (error) {
             console.error("Error executing MySQL query:", error);
             res.status(500).json({ error: "Internal Server Error" });
             return;
         }
-        const jugador = results;
+        const jugador = results.rows;
         res.status(200).json({ jugador });
     });
 });
-*/
 
-// Conseguir id con nombre de jugador
-/*
-app.get('/api/v1/jugadores/:nombre', (req, res) => {
-    const nombre = req.params.nombre;
-    const query = `SELECT jugador_id
-                 FROM jugadores 
-                 WHERE nombre = ${nombre}`;
+// Crea un jugador en la BD
+app.post('/api/v1/post/jugador', (req, res) => {
+    const {
+        id, nombre, propietario, equipo, posicion, titular, partidos_jugados, ranking_general, ranking_equipo,
+        ranking_posicion, media_sofascore, media_puntos, total_puntos, valor_mercado, valor_mercado_max,
+        valor_mercado_min, tarjeta_amarilla, tarjeta_roja, doble_tarjeta_amarilla, racha, lesion
+    } = req.body;
 
-    client.query(query, (error, results) => {
+    const query = `INSERT INTO jugadores (id, nombre, propietario, equipo, posicion, titular, partidos_jugados,
+                    ranking_general, ranking_equipo, ranking_posicion, media_sofascore, media_puntos, total_puntos,
+                    valor_mercado, valor_mercado_max, valor_mercado_min, tarjeta_amarilla, tarjeta_roja, doble_tarjeta_amarilla, racha, lesion)
+                VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21)`;
+
+    const values = [
+        id, nombre, propietario, equipo, posicion, titular, partidos_jugados, ranking_general, ranking_equipo,
+        ranking_posicion, media_sofascore, media_puntos, total_puntos, valor_mercado, valor_mercado_max,
+        valor_mercado_min, tarjeta_amarilla, tarjeta_roja, doble_tarjeta_amarilla, racha, lesion
+    ]
+
+    client.query(query, values, (error, results) => {
         if (error) {
             console.error("Error executing MySQL query:", error);
             res.status(500).json({ error: "Internal Server Error" });
             return;
         }
-        const jugador = results;
-        res.status(200).json({ jugador });
+        res.status(200).json({ mensaje: "Jugador creado" });
     });
 });
 
-// Buscar jugadores por id
-app.get('/api/v1/jugadores/:id', (req, res) => {
+// Get del pack "puntos" para el jugador por id
+app.get('/api/v1/jugador/:id/puntos', (req, res) => {
     const id = req.params.id;
-    const query = `SELECT * FROM jugadores WHERE id_jugador = ${id}`;
+    const query = 'SELECT nombre, media_softscore, media_puntos, total_puntos, puntos_buenos FROM jugadores WHERE id_jugador = $1';
+    const values = [id];
+    client.query(query, values, (error, results) => {
+        if (error) {
+            console.error("Error executing MySQL query:", error);
+            res.status(500).json({ error: "Internal Server Error" });
+            return;
+        }
+        const jugadores = results.rows;
+        res.status(200).json({ jugadores });
+    });
+});
+
+// Get el historial de un jugador por id especificando dias
+app.get('/api/v1/historial_jugador/:id/dias/:dias', (req, res) => {
+    const id = req.params.id;
+    const dias = req.params.dias;
+    const query = 'SELECT * FROM historial_jugadores WHERE id_jugador = $1 ORDER BY id_historial_jugador DESC LIMIT $2;';
+    const values = [id, dias];
+    client.query(query, values, (error, results) => {
+
+        if (error) {
+            console.error("Error executing MySQL query:", error);
+            res.status(500).json({ error: "Internal Server Error" });
+            return;
+        }
+        const jugadores = results.rows;
+        res.status(200).json({ jugadores });
+    });
+});
+
+// Get del historial de un jugador por id especificando dias el paquete general
+app.get('/api/v1/historial_jugador/:id/general/dias/:dias', (req, res) => {
+    const id = req.params.id;
+    const dias = req.params.dias;
+    const query = 'SELECT nombre, equipo, posicion, titular, partidos_jugados, racha, fecha_registro FROM historial_jugadores WHERE id_jugador = $1 ORDER BY id_historial_jugador DESC LIMIT $2;'
+    const values = [id, dias];
+    client.query(query, values, (error, results) => {
+        if (error) {
+            console.error("Error executing MySQL query:", error);
+            res.status(500).json({ error: "Internal Server Error" });
+            return;
+        }
+        const jugadores = results.rows;
+        res.status(200).json({ jugadores });
+    });
+});
+
+// Get del historial de un jugador por id especificando dias el paquete mercado valor
+app.get('/api/v1/historial_jugador/:id/mercado_valor/dias/:dias', (req, res) => {
+    const id = req.params.id;
+    const dias = req.params.dias;
+    const query = 'SELECT nombre, valor_mercado, fecha_registro FROM historial_jugadores WHERE id_jugador = $1 ORDER BY id_historial_jugador DESC LIMIT $2;'
+    const values = [id, dias];
+    client.query(query, values, (error, results) => {
+        if (error) {
+            console.error("Error executing MySQL query:", error);
+            res.status(500).json({ error: "Internal Server Error" });
+            return;
+        }
+        const jugadores = results.rows;
+        res.status(200).json({ jugadores });
+    });
+});
+
+// Get del historial de un jugador por id especificando dias el paquete puntos
+app.get('/api/v1/historial_jugador/:id/puntos/dias/:dias', (req, res) => {
+    const id = req.params.id;
+    const dias = req.params.dias;
+    const query = 'SELECT nombre, media_softscore, media_puntos, total_puntos, fecha_registro FROM historial_jugadores WHERE id_jugador = $1 ORDER BY id_historial_jugador DESC LIMIT $2;'
+    const values = [id, dias];
+    client.query(query, values, (error, results) => {
+        if (error) {
+            console.error("Error executing MySQL query:", error);
+            res.status(500).json({ error: "Internal Server Error" });
+            return;
+        }
+        const jugadores = results.rows;
+        res.status(200).json({ jugadores });
+    });
+});
+
+// Get del historial de un jugador por id especificando dias el paquete estadistica
+app.get('/api/v1/historial_jugador/:id/estadistica/dias/:dias', (req, res) => {
+    const id = req.params.id;
+    const dias = req.params.dias;
+    const query = 'SELECT nombre, ranking_general, ranking_equipo, ranking_posicion, tarjeta_amarilla, doble_tarjeta_amarilla, tarjeta_roja FROM historial_jugadores WHERE id_jugador = $1 ORDER BY id_historial_jugador DESC LIMIT $2;'
+    const values = [id, dias];
+    client.query(query, values, (error, results) => {
+        if (error) {
+            console.error("Error executing MySQL query:", error);
+            res.status(500).json({ error: "Internal Server Error" });
+            return;
+        }
+        const jugadores = results.rows;
+        res.status(200).json({ jugadores });
+    });
+});
+
+// Get un campo del historial de un jugador por id especificando dias el paquete estadistica
+app.get('/api/v1/historial_jugador/:id/:campo/dias/:dias', (req, res) => {
+    const id = req.params.id;
+    const campo = req.params.campo;
+    const dias = req.params.dias;
+    const query = 'SELECT $2 FROM historial_jugadores WHERE id_jugador = $1 ORDER BY id_historial_jugador DESC LIMIT $3;';
+    const values = [id, campo, dias];
+    client.query(query, values, (error, results) => {
+        if (error) {
+            console.error("Error executing MySQL query:", error);
+            res.status(500).json({ error: "Internal Server Error" });
+            return;
+        }
+        const jugadores = results.rows;
+        res.status(200).json({ jugadores });
+    });
+});
+
+// Get al mejor fichaje de todos los que hay
+app.get('/api/v1/mejor_fichaje', (req, res) => {
+    const query = 'SELECT * FROM jugadores WHERE mejor_fichaje = true';
     client.query(query, (error, results) => {
         if (error) {
             console.error("Error executing MySQL query:", error);
             res.status(500).json({ error: "Internal Server Error" });
             return;
         }
-        const jugador = results;
-        res.status(200).json({ jugador });
+        const jugadores = results.rows;
+        res.status(200).json({ jugadores });
     });
 });
 
-
-
-
-// // // Buscar jugadores por nombre
-// // app.get('/api/v1/jugadores/:nombre', (req, res) => {
-// //     const { nombre } = req.params;
-// //     const query = `SELECT * FROM jugadores WHERE nombre = '${nombre}'`;
-// //     connection.query(query, (error, results) => {
-// //         if (error) {
-// //             console.error("Error executing MySQL query:", error);
-// //             res.status(500).json({ error: "Internal Server Error" });
-// //             return;
-// //         }
-// //         const jugador = results;
-// //         res.status(200).json({ jugador });
-// //     });
-// // });
-
-// app.post('/api/v1/jugadores', (req, res) => {
-//     const { id, nombre, precio, puntos_totales, equipo } = req.body;
-//     const query = `INSERT INTO jugadores ( id, nombre, precio, puntos_totales, equipo) VALUES ('${id}', '${nombre}', '${precio}', '${puntos_totales}', '${equipo}')`;
-//     connection.query(query, (error, results) => {
-//         if (error) {
-//             console.error("Error executing MySQL query:", error);
-//             res.status(500).json({ error: "Internal Server Error" });
-//             return;
-//         }
-//         const jugador = results;
-//         res.status(200).json({ id: id, nombre: nombre, precio: precio });
-//     });
-// });
-
-*/
 app.listen(80, () => {
     console.log(`Server running on port ${80}`);
 });
